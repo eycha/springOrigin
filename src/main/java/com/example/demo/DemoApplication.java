d67ae8cefd552e0b2930c575ef2c1dca407b85fd
eycha <silvlexonst@gmail.com>
2023-03-11 18:50:51 +0900

    proxy pattern aop

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.adapter.*;
+import com.example.demo.aop.AopBrowser;
 import com.example.demo.proxy.Browser;
 import com.example.demo.proxy.BrowserProxy;
 import com.example.demo.proxy.IBrowser;
@@ -10,6 +11,8 @@ import com.example.demo.singleTon.SocketClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.converter.json.GsonBuilderUtils;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -43,16 +46,34 @@ public class DemoApplication {
         browser.show();
         browser.show();
 
-        */
+
 
         IBrowser browser = new BrowserProxy("www.naver.com");
         browser.show();
         browser.show();
         browser.show();
         browser.show();
+    */
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
 
-    }
-    public static void connect(Electronic110V electronic110V) {
-        electronic110V.powerOn();
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+
+        );
+
+        aopBrowser.show();
+        System.out.println("loading time :"+ end.get());
+
+        aopBrowser.show();
+        System.out.println("loadung time :"+ end.get());
+
     }
 }
